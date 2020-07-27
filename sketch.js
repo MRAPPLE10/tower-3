3b54204d587dee0ab0e98c66297089970f9bbeff
MRAPPLE10 <65347112+MRAPPLE10@users.noreply.github.com>
2020-07-27 17:35:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1;
+var backgroundImg,platform;
+var bird, slingShot;
+var gameState = "onSling";
+var bg = "sprites/bg1.png";
+var score = 0;
+function preload() {
+    
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(770,320,70,70);
+    box3 = new Box(840,320,70,70);
+    box4 = new Box(910,320,70,70);
+    box5 = new Box(735,240,70,70);
+    box6 = new Box(805,240,70,70);
+    box7 = new Box(880,160,70,70);
+     bird = new Bird(100,100);
+slingshot = new SlingShot(bird.body,{x:200,y:100});
+}
+
+function draw(){
+
+    
+    
+    Engine.update(engine);
+    strokeWeight(4);
+    box1.display();
+    box2.display();
+    box3.display();
+    box4.display();
+    box5.display();
+    box6.display();
+    box7.display();
+    bird.display();
+    platform.display();
+    slingshot.display();    
+    ground.display();
+}
+function mouseDragged(){
+    //if (gameState!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    //}
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+    gameState = "launched";
+}
